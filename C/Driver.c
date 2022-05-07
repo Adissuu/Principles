63cc57cf8107857be411870fd983a1046dbc20e6
Gabriel <gabrieldubois.eng@gmail.com>
2022-05-07 13:52:28 -0400

    ca part

--- C/Driver.c
+++ C/Driver.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "AGGREGATE.h"
+#define WINPAUSE system("pause")
 
 void pointers(float *five_numbers, float *ten_numbers)
 {
@@ -11,9 +12,18 @@ void pointers(float *five_numbers, float *ten_numbers)
     for (i = 0; i < 5; i++)
     {
         result = aggregates[i](five_numbers, 5);
-        printf("Result of the current function on the array of five numbers: %.2f.\n\n", result);
+        printf("Result of the function #%d on the array of five numbers: %.2f.\n\n", i, result);
 
         result = aggregates[i](ten_numbers, 10);
-        printf("Result of the current function on the array of ten numbers: %.2f.\n\n", result);
+        printf("Result of the function #%i on the array of ten numbers: %.2f.\n\n", i, result);
     }
+}
+
+void main()
+{
+    float five_numbers[5] = {6, 7, 5, 4, 3};
+    float ten_numbers[10] = {10, 20, 41, 21, 56, 234, 67, 87, 99, 76};
+
+    pointers(five_numbers, ten_numbers);
+    WINPAUSE;
 }
