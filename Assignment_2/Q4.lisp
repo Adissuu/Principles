a9a8450652ea9f5da9782b3e22e82313b112f8fa
Gabriel <gabrieldubois.eng@gmail.com>
2022-05-22 15:46:41 -0400

    q5

---
+++ Assignment_2/Q4.lisp
@@ -0,0 +1,27 @@
+(defun reversing (list)
+    (cond 
+        ((null list) '())
+        (t (append (reversing (cdr list)) (list (car list)))))
+)
+
+(defun no-dup (list)
+    (setf tempList list)
+    (setf list (remove (car tempList) tempList))
+    (setf li (list (car tempList) (car (cdr tempList))))
+    (setf list (append li (remove (car list) list)))
+    (setf list (remove NIL list))
+)
+
+(defun flatten-nums-nodup (list) 
+        (no-dup (cond 
+            ((null list)
+                list)
+            ((listp (car list))
+                (append (flatten-nums-nodup (car list)) (flatten-nums-nodup (cdr list))))
+            ((and (not (numberp (car list))) (not (listp (car list)))) 
+                (flatten-nums-nodup (cdr list)))
+            (t
+                (cons (car list) (flatten-nums-nodup (cdr list)))
+            ))))
+
+(print (flatten-nums-nodup '(1 2 (3 1) (a 2.5) (2 4.5) ((1 2)))))
---
+++ Assignment_2/Q5.lisp
@@ -0,0 +1,12 @@
+(defun reverseList (list)
+    (cond 
+        ((null list) '())
+        (t (append (reverseList (cdr list)) (list (car list))))))
+
+(defun tribonacci-seq (number)
+    (setq zero 0)
+    (setq zero2 0)
+    (setq one 1)
+    (cond 
+        )
+)
