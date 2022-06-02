de78167c836ed349c2b3978b4368d2b40857e9df
Fetyali7 <70493307+Fetyali7@users.noreply.github.com>
2022-06-01 22:19:38 -0400

    Add files via upload

---
+++ Assignment_3/Q)11.pl
@@ -0,0 +1,20 @@
+method(0,2) :- !.
+method(1,1) :- !.
+method(N,R) :-
+    N > 1,
+    N_pre os n-1,
+    N_PrePrevious is n-2,
+    method(N_pre, LHS),
+    method(N_PrePrevious, RHS),
+    R is LHS + RHS.
+
+% Memorization of Lucas numbers
+:- table lucas/2.
+
+% Generate List
+Lucasseq(M,Result) :-
+  numlist(0,M, List),
+  Maplist(method, List, Result).
+
+
+ 
