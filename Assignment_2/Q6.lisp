dfc800d7f445e60cd3b96580d43915fb2f5c378b
Gabriel Dubois <90948686+Adissuu@users.noreply.github.com>
2022-06-02 11:05:01 -0400

    Update Q6.lisp

--- Assignment_2/Q6.lisp
+++ Assignment_2/Q6.lisp
@@ -4,4 +4,6 @@
         ((listp list) (+ 1 (reduce #'max (mapcar #'depth list))))
         (t (return-from depth 0))))
 
+(print (depth '(2)))
+(print (depth '((2))))
 (print (depth '((2)(3 (6))(4))))
